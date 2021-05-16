191fa5f922d3a0351d2bab63336dee28fd17f6cb
piplupOwo <82926705+piplupOwo@users.noreply.github.com>
2021-05-16 18:47:50 +0800

    Add files via upload

---
+++ piplup/piplupConfig.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2018, Tomas Slusny <slusnucky@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.piplup;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+
+@ConfigGroup("piplup")
+public interface piplupConfig extends Config
+{
+	// Configuration menu entries go here
+
+
+    @ConfigItem
+            (
+                    position = 1,
+                    keyName = "booleansoundConfig",
+                    name = "sound every tick",
+                    description = "plays mining sound every tick"
+            )
+    default boolean booleansoundConfig() { return false; }
+
+    @ConfigItem
+            (
+                    position = 2,
+                    keyName = "booleanleechConfig",
+                    name = "Play leech sound when scythe without max stat",
+                    description = "plays the sound"
+            )
+    default boolean booleanleechConfig() { return false; }
+
+    @ConfigItem
+            (
+                    position = 3,
+                    keyName = "booleanpipConfig",
+                    name = "Play pip sound when scythe without max stat",
+                    description = "plays the sound"
+            )
+    default boolean booleanpipConfig() { return false; }
+
+
+    @ConfigItem(
+            keyName = "leechSoundFilePath",
+            name = "leech .wav file path",
+            description = "The path to the file to be used for \"leech\" sounds (short .wav only)"
+    )
+    default String leechPath()
+    {
+        return "";
+    }
+
+    @ConfigItem(
+            keyName = "pipSoundFilePath",
+            name = "pip .wav file path",
+            description = "The path to the file to be used for \"pip\" sounds (short .wav only)"
+    )
+    default String pipPath()
+    {
+        return "";
+    }
+
+    @ConfigItem(
+            keyName = "volume",
+            name = "Volume modification optimal at 60 idk whats the max but anything below 60 should work well",
+            description = "Configures leech volume; only effects custom sounds."
+    )
+    default int volume()
+    {
+        return 60;
+    }
+
+
+}
+
